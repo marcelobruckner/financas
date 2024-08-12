e8d8093cf4cd015442222ebdd9eeb821d53133e0
Marcelo Bruckner <marcelobruckner@gmail.com>
2024-08-12 15:32:12 -0300

    update api

--- src/main/java/com/bruckner/financas/api/FinancasApi.java
+++ src/main/java/com/bruckner/financas/api/FinancasApi.java
@@ -7,15 +7,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/api/")
+// @RequestMapping("/api/")
 public class FinancasApi {
 
     private List<String> cursos = Arrays.asList("Investimento", "CDB", "Tesouro Direto");
 
-    @GetMapping("cursos")
+    @GetMapping("/")
     public List<String> list() {
         System.out.println(new Date());
         return cursos;
