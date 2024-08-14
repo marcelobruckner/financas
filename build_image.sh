5f83f9f0ee2026960c6c96534fe0050179d3ada1
Marcelo Bruckner <marcelobruckner@gmail.com>
2024-08-14 14:26:38 -0300

    preparando app para receber vairiaveis de ambiente

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
-podman build -t marcelobruckner/financas-api:3.1 -f Containerfile
+podman build -t marcelobruckner/financas-api:3.2 -f Containerfile
 #podman build -f Containerfile --tag docker.io/marcelobruckner/financas-api:2.0
