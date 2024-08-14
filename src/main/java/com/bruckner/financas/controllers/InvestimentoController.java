4ecb3c4ccd253917d56af727dde601a21501e2d1
Marcelo Bruckner <marcelobruckner@gmail.com>
2024-08-14 11:29:34 -0300

    Retirando imports e logs nao utilizados

--- src/main/java/com/bruckner/financas/controllers/InvestimentoController.java
+++ src/main/java/com/bruckner/financas/controllers/InvestimentoController.java
@@ -1,14 +1,11 @@
 package com.bruckner.financas.controllers;
 
 import com.bruckner.financas.models.InvestimentoModel;
-import com.bruckner.financas.repositories.InvestimentoRepository;
 import com.bruckner.financas.service.InvestimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,7 +22,6 @@ public class InvestimentoController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public InvestimentoModel adicionar(@RequestBody InvestimentoModel model) {
-        System.out.println(model.getNome());
         return investimentoService.adicionar(model);
     }
     
