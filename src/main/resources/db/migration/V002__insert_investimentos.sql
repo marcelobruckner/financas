c8766d4f550c2eff217549a0aac7cf9ef5875746
Marcelo Bruckner <marcelobruckner@gmail.com>
2024-09-11 12:12:06 -0300

    atualizacao do pom e insert em tabelas

---
+++ src/main/resources/db/migration/V002__insert_investimentos.sql
@@ -0,0 +1,5 @@
+insert into investimentos (nome) values ('CBD');
+insert into investimentos (nome) values ('Poupanca');
+insert into investimentos (nome) values ('Tesouro direto');
+insert into investimentos (nome) values ('FGTS');
+insert into investimentos (nome) values ('Previdencia privada');
