dd3d31696bf30ca1bd144d2cfe9c10280a74116e
Marcelo Bruckner <marcelobruckner@gmail.com>
2024-09-20 19:44:04 -0300

    melhorias no projeto

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
-podman build -t marcelobruckner/financas-api:3.2 -f Containerfile
+podman build -t marcelobruckner/financas-api:3.3 -f Containerfile
 #podman build -f Containerfile --tag docker.io/marcelobruckner/financas-api:2.0
--- run_image.sh
+++ run_image.sh
@@ -1,3 +1,3 @@
 podman stop podman_financas
 podman rm podman_financas
-podman run -d --name podman_financas -p 8080:8080 marcelobruckner/financas-api
+podman run -d --name podman_financas -p 8080:8080 -e DATABASE_HOST=mysql-db -e DATABASE_NAME=financas --network financas-ntw marcelobruckner/financas-api:3.3
